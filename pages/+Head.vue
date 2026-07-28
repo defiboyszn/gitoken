1e909c9eed38be0cad87dfe318cac65c92b50e06
Defiboy <tobithealpha@gmail.com>
2026-07-28 18:32:38 +0100

    Added Meta Verification tag

--- pages/+Head.vue
+++ pages/+Head.vue
@@ -4,6 +4,7 @@
   <link rel="icon" type="image/png" :href="faviconUrl" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <meta name="ory-verify" content="orynth-52f0b81c0cce4fc3b58dba2c578d86e9" />
   <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=Syne:wght@600;800&display=swap" rel="stylesheet" />
 </template>
 
